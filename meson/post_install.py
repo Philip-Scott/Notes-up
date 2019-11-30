5b88db307825a6e9728b87d532c61f5ff0ba3f65
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2019-11-30 15:51:33 +0100

    Improve post_install.py script

--- meson/post_install.py
+++ meson/post_install.py
@@ -6,9 +6,12 @@ import subprocess
 prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
 schemadir = path.join(environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
 datadir = path.join(prefix, 'share')
+desktop_database_dir = path.join(datadir, 'applications')
 
 if not environ.get('DESTDIR'):
     print('Compiling gsettings schemas…')
     subprocess.call(['glib-compile-schemas', schemadir])
+    print('Updating desktop database…')
+    subprocess.call(['update-desktop-database', '-q', desktop_database_dir])
     print('Updating icon cache…')
     subprocess.call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
